236900ba7d025f0f63b3d36b3060908d1f0b6311
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-07-20 23:39:03 +0530

    radius

---
+++ radius.cpp
@@ -0,0 +1,11 @@
+#include<iostream>
+using namespace std;
+int  main()
+{
+    int r,h,c;
+    cout<<"enter the first and second no";
+    cin>>r>>h;
+   c=3.14*r*r*h;
+   cout<<r<<"\n"<<h<<"\n"<<"sum="<<c;
+   return 0;
+}
