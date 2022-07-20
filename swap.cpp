0b5e2d45075c1d5e92e97c586917923c59a4ec83
Ram Prasann Pandey <106461823+Ramprasann@users.noreply.github.com>
2022-07-20 23:39:25 +0530

    swapping

---
+++ swap.cpp
@@ -0,0 +1,13 @@
+#include<iostream>
+using namespace std;
+int main()
+{
+    int a,b,c;
+    cout<<"enter the no";
+    cin>>a>>b;
+    c=a;
+    a=b;
+    b=c;
+    cout<<"a="<<a<<"\n"<<"b="<<b;
+
+}
